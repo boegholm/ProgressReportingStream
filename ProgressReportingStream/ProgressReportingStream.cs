f8ffe82d8dc0c01521473fcac64f563caed3e696
Thomas Bøgholm <github.com@glirc.dk>
2021-07-16 06:49:09 +0200

    Add project files.

---
+++ ProgressReportingStream/ProgressReportingStream.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace Utils
+{
+    public delegate void StreamUpdate(Stream sender, long progress, long totalProgress);
+    public class ProgressReportingStream : Stream
+    {
+        public event StreamUpdate WriteProgress;
+        public event StreamUpdate ReadProgress;
+        long totalBytesRead;
+        long totalBytesWritten;
+        private void UpdateRead(long read)
+        {
+            totalBytesRead += read;
+            ReadProgress?.Invoke(this, read, totalBytesRead);
+        }
+        private void UpdateWritten(long written)
+        {
+            totalBytesWritten += written;
+            WriteProgress?.Invoke(this, written, totalBytesWritten);
+        }
+
+        Stream InnerStream { get; init; }
+
+        public override bool CanRead => InnerStream.CanRead;
+
+        public override bool CanSeek => InnerStream.CanSeek;
+
+        public override bool CanWrite => InnerStream.CanWrite;
+
+        public override long Length => InnerStream.Length;
+
+        public override long Position { get => InnerStream.Position; set => InnerStream.Position = value; }
+
+        public ProgressReportingStream(Stream s)
+        {
+            this.InnerStream = s;
+        }
+
+        public override void Flush()
+        {
+            InnerStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var result = InnerStream.Read(buffer, offset, count);
+            UpdateRead(result);
+            return result;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return InnerStream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            InnerStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            InnerStream.Write(buffer, offset, count);
+            UpdateWritten(count);
+        }
+    }
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using Utils;
+
+namespace Sample
+{
+    class Program
+    {
+        private static void WriteSample(MemoryStream ms)
+        {
+            using var rs = new ProgressReportingStream(ms);
+            rs.WriteProgress += Rs_WriteProgress;
+
+            using var sw = new StreamWriter(rs, leaveOpen: true) { AutoFlush = true };
+            sw.WriteLine("Hello");
+            sw.WriteLine("ProgressStreaming");
+            sw.WriteLine("World!");
+        }
+        static void ReadSample(Stream ms)
+        {
+            ms.Seek(0, SeekOrigin.Begin);
+            using var rs = new ProgressReportingStream(ms);
+            rs.ReadProgress += Rs_ReadProgress;
+            using var sr = new StreamReader(rs, leaveOpen: true);
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+            Console.WriteLine(sr.ReadLine());
+        }
+
+        private static void CopySample(MemoryStream ms)
+        {
+            ms.Seek(0, SeekOrigin.Begin);
+            using var reportingStream = new ProgressReportingStream(ms);
+            reportingStream.ReadProgress += Rs_ReadProgress;
+            using MemoryStream os = new MemoryStream();
+            using var pros = new ProgressReportingStream(os);
+            pros.WriteProgress += Rs_WriteProgress;
+            reportingStream.CopyTo(pros);
+        }
+
+        static void Main(string[] args)
+        {
+            using var ms = new MemoryStream();
+
+            DisplaySample(()=>WriteSample(ms), "Write sample");
+            DisplaySample(()=>ReadSample(ms), "Read sample");
+            DisplaySample(()=>CopySample(ms), "Copy sample");
+        }
+
+        private static void Rs_WriteProgress(Stream sender, long progress, long totalProgress)
+        {
+            Console.WriteLine($"Status: {progress} bytes written ({totalProgress} total)");
+        }
+
+        private static void Rs_ReadProgress(Stream sender, long progress, long totalProgress)
+        {
+            Console.WriteLine($"Status: {progress} bytes read ({totalProgress} total)");
+        }
+        static void DisplaySample(Action sample, string heading)
+        {
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"[[{heading}]]");
+            sample();
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine();
+        }
+    }
+}
